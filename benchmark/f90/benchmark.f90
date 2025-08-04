f950d787e24a65ee7f2c298adacb060adba8859d
aredjil <bellred499@gmail.com>
2025-08-04 13:04:59 +0200

    updated the benchmarking code

--- benchmark/f90/benchmark.f90
+++ benchmark/f90/benchmark.f90
@@ -3,7 +3,8 @@ program benchmark
     ! Calling the netcdf library to use it for opening and reading the data from the file 
     use netcdf 
     implicit none
-
+    !----------------------------
+    logical :: error
     ! netCDF variables section
     !------------------------------------------------------------------------------------ 
     ! Netcdf variables 
@@ -13,13 +14,40 @@ program benchmark
     !---------------------
     integer, parameter :: n_iter = 300, n_points = 412300, n_waves = 33 
     
-    ! Variables to read
+    ! Variables to read from NetCDF
     !-------------------
     integer, allocatable :: iyr(:), iday(:)
     real(kind=real_kind), allocatable ::  sec_b(:), sec_e(:)
     real(kind=real_kind), allocatable ::  sp(:,:), msl(:,:), ws10(:,:), tco3(:,:), t2m(:,:)
     real(kind=real_kind), allocatable ::  d2m(:,:), tcc(:,:), tclw(:,:), cdrem(:,:)
     real(kind=real_kind), allocatable ::  taua(:,:,:), asymp(:,:,:), ssalb(:,:,:)
+    real(kind=real_kind), allocatable ::  lat(:), lon(:)
+
+    ! Variables for OASIM computation
+    !--------------------------------
+    integer, dimension(:), allocatable :: points
+    real(kind=real_kind), dimension(:), allocatable :: slp, wsm, oz, wv, rh, ccov, rlwp, cdre
+    real(kind=real_kind), dimension(:,:), allocatable :: edout, esout
+    real(kind=real_kind), dimension(:,:), allocatable :: taua_slice, asymp_slice, ssalb_slice
+    
+    ! Physical constants
+    !-------------------
+    real(kind=real_kind), parameter :: a1 = 611.21d0 ! Pascal
+    real(kind=real_kind), parameter :: a3 = 17.502d0 ! dimensionless
+    real(kind=real_kind), parameter :: a4 = 32.19d0  ! Kelvin
+    real(kind=real_kind), parameter :: To = 273.16d0 ! Kelvin
+    real(kind=real_kind), parameter :: b1 = 0.14d0 * 0.01d0  ! cm/Pascal
+    real(kind=real_kind), parameter :: b2 = 0.21d0   ! cm
+
+    ! Working variables
+    !------------------
+    real(kind=real_kind) :: T, Td, es_Td, es_T
+    integer :: i, j, iter
+    
+    ! OASIM objects
+    !--------------
+    type(oasim_lib) :: lib
+    type(calc_unit) :: calc
 
     ! Allocating memory for the variables 
     !------------------------------------
@@ -31,302 +59,168 @@ program benchmark
     allocate(taua(n_points, n_waves, n_iter))
     allocate(asymp(n_points, n_waves, n_iter))
     allocate(ssalb(n_points, n_waves, n_iter))
+    allocate(lat(n_points), lon(n_points))
 
-
-
-    
-    ! File path (must be modified on Leonardo to correspond to where the data is)
-    !------------
-    ! character (len=100), parameter:: FILE_NAME ="/home/aredjil/projects/OASIM_ATM/data000.nc"
-
-
-    ! logical :: error
-    ! integer :: year, day !,i
-    ! real(kind=real_kind) :: sec_b, sec_e
-    ! integer, parameter :: m_size = 1
-    ! integer, parameter :: p_size = 1
-    ! real(kind=real_kind), parameter :: a1 = 611.21d0 ! Pascal
-    ! real(kind=real_kind), parameter :: a3 = 17.502d0 ! dimensionless
-    ! real(kind=real_kind), parameter :: a4 = 32.19d0  ! Kelvin
-    ! real(kind=real_kind), parameter :: To = 273.16d0 ! Kelvin
-    ! real(kind=real_kind), parameter :: b1 = 0.14d0 * 0.01d0  ! cm/Pascal
-    ! real(kind=real_kind), parameter :: b2 = 0.21d0   ! cm
-    ! real(kind=real_kind)                            :: sp, msl,d2m,t2m,tcc,ws10,tclw,tco3,cdrem,cldtcm,T,Td,es_Td,es_T
-    ! real(kind=real_kind), dimension(:), allocatable :: lat, lon
-    ! integer, dimension(:), allocatable :: points
-    ! real(kind=real_kind), dimension(:), allocatable :: slp, wsm, oz, wv, rh, ccov, rlwp, cdre
-    ! real(kind=real_kind), dimension(:,:), allocatable :: taua, asymp, ssalb, edout, esout
-    
-
-
-
-    ! type(oasim_lib) :: lib
-    ! type(calc_unit) :: calc
-
-    ! allocate(lat(m_size))
-    ! allocate(lon(m_size))
-
-    ! do i = 1, m_size
-    !     lat(i) = float(i) * 0.25d0 - 3.0d0
-    !     lon(i) = 40.0d0 + float(i) * 0.10d0
-    ! end do
-
-    ! lon=7.2916665
-    ! lat=42.9375
-
-    ! lib = oasim_lib("config.yaml", lat, lon, error)
-
-    ! if (error) then
-    !     write(*, *) "Something wrong happened."
-    !     stop 1
-    ! end if
-
-    ! write(*, *) "Configuration file correctly read:"
-    ! write(*, *) " "
-    ! write(*, *) trim(lib%init_parameters%atmo_file)
-    ! write(*, *) trim(lib%init_parameters%abso_file)
-    ! write(*, *) trim(lib%init_parameters%slingo_file)
-    ! write(*, *) lib%init_parameters%integration_step_secs
-    ! write(*, *) lib%init_parameters%max_integration_steps
-    ! write(*, *) lib%init_parameters%zenith_avg
-    ! write(*, *) lib%init_parameters%local_time
-    ! write(*, *) trim(lib%init_parameters%bin_file)
-    ! write(*, *) " "
-    ! write(*, *) " "
-    ! write(*, *) " "
-    ! write(*, *) "Tables read correctly."
-    ! write(*, *) " "
-    ! write(*, *) " "
-    ! write(*, *) "ATMO"
-    ! write(*, *) " "
-    ! call lib%atmo_adapted%print()
-    ! write(*, *) " "
-    ! write(*, *) " "
-    ! write(*, *) "ABSO"
-    ! write(*, *) " "
-    ! call lib%abso_adapted%print()
-    ! write(*, *) " "
-    ! write(*, *) " "
-    ! write(*, *) "SLINGO"
-    ! write(*, *) " "
-    ! call lib%slingo_adapted%print()
-    ! write(*, *) " "
-    ! write(*, *) " "
-    ! The below line was commented before me commenting leave it comented 
-    ! write(*, *) lib%m_size
-    ! write(*, *) " "
-    ! do i = 1, lib%m_size
-    !     write(*, *) lib%lat(i), lib%lon(i), lib%up(i,:), lib%no(i,:), lib%ea(i,:)
-    ! end do
-
-    ! allocate(slp(p_size))
-    ! allocate(wsm(p_size))
-    ! allocate(oz(p_size))
-    ! allocate(wv(p_size))
-    ! allocate(rh(p_size))
-    ! allocate(ccov(p_size))
-    ! allocate(rlwp(p_size))
-    ! allocate(cdre(p_size))
-    ! allocate(points(p_size))
-
-    ! write(*, *) lib%rows, p_size
-
-    ! allocate(taua(p_size, lib%rows))
-    ! allocate(asymp(p_size, lib%rows))
-    ! allocate(ssalb(p_size, lib%rows))
-    ! allocate(edout(p_size, lib%rows))
-    ! allocate(esout(p_size, lib%rows))
-
-    ! Opening the netcdf file 
-    !-------------------------
+    ! Opening the netcdf file and reading data
+    !-----------------------------------------
     call check(nf90_open("/home/aredjil/projects/OASIM_ATM/data000.nc", NF90_NOWRITE, ncid))
-    ! Reading  iyr
-    !-----------------
+    
+    ! Reading time variables
     call check(nf90_inq_varid(ncid, "iyr", varid))
     call check(nf90_get_var(ncid, varid, iyr))
-    !Reading iday
-    !------------
-    ! Reading  sp 
     call check(nf90_inq_varid(ncid, "iday", varid))
     call check(nf90_get_var(ncid, varid, iday))
+    ! Reading the start and end seconds of the day     
+    ! sec_b = 28800.0d0  ! 8 AM in seconds
+    ! sec_e = 36000.0d0  ! 10 AM in seconds
+    call check(nf90_inq_varid(ncid, "sec_b", varid))
+    call check(nf90_get_var(ncid, varid, sec_b))
+    call check(nf90_inq_varid(ncid, "sec_e", varid))
+    call check(nf90_get_var(ncid, varid, sec_e))
 
-    !-------------------------
+
+    ! call check(nf90_inq_varid(ncid, "lat", varid))
+    ! call check(nf90_get_var(ncid, varid, lat))
+    ! call check(nf90_inq_varid(ncid, "lon", varid))
+    ! call check(nf90_get_var(ncid, varid, lon))
+
+    ! Reading atmospheric variables
     call check(nf90_inq_varid(ncid, "sp", varid))
     call check(nf90_get_var(ncid, varid, sp))
-    ! Reading  msl 
-    !---------------------------------------
     call check(nf90_inq_varid(ncid, "msl", varid))
     call check(nf90_get_var(ncid, varid, msl))
-    ! Reading ws10
-    !------------
     call check(nf90_inq_varid(ncid, "ws10", varid))
     call check(nf90_get_var(ncid, varid, ws10))
-    ! Reading tco3
-    !------------
     call check(nf90_inq_varid(ncid, "tco3", varid))
     call check(nf90_get_var(ncid, varid, tco3))
-    ! Reading t2m
-    !------------
     call check(nf90_inq_varid(ncid, "t2m", varid))
     call check(nf90_get_var(ncid, varid, t2m))
-    ! Reading d2m
-    !------------
     call check(nf90_inq_varid(ncid, "d2m", varid))
     call check(nf90_get_var(ncid, varid, d2m))
-    ! Reading tcc
-    !------------
     call check(nf90_inq_varid(ncid, "tcc", varid))
     call check(nf90_get_var(ncid, varid, tcc))
-    ! Reading tclw
-    !------------
     call check(nf90_inq_varid(ncid, "tclw", varid))
     call check(nf90_get_var(ncid, varid, tclw))
-    ! Reading cdrem
-    !------------
     call check(nf90_inq_varid(ncid, "cdrem", varid))
     call check(nf90_get_var(ncid, varid, cdrem))
-    ! Reading taua
-    !------------
     call check(nf90_inq_varid(ncid, "taua", varid))
     call check(nf90_get_var(ncid, varid, taua))
-    ! Reading asymp
-    !------------
     call check(nf90_inq_varid(ncid, "asymp", varid))
     call check(nf90_get_var(ncid, varid, asymp))
-    ! Reading ssalb
-    !------------
     call check(nf90_inq_varid(ncid, "ssalb", varid))
     call check(nf90_get_var(ncid, varid, ssalb))
 
-    ! Prinitng the year for testing 
-    !------------------------------
-    ! print *, "The years in the file are: "
-    ! print *, iyr 
+    ! Close the NetCDF file
+    call check(nf90_close(ncid))
+
+    write(*, *) "NetCDF data read successfully"
+    write(*, *) "Number of points: ", n_points
+    write(*, *) "Number of iterations: ", n_iter
+    write(*, *) "Number of wavelengths: ", n_waves
+
+    ! Initialize OASIM library
+    !-------------------------
+    lib = oasim_lib("config.yaml", lat, lon, error)
     
-    ! print *, "Printing sp: "
-    ! print *, sp
+    if (error) then
+        write(*, *) "Error initializing OASIM library"
+        stop 1
+    end if
 
+    write(*, *) "OASIM library initialized successfully"
 
+    ! Allocate working arrays
+    !------------------------
+    allocate(points(n_points))
+    allocate(slp(n_points), wsm(n_points), oz(n_points))
+    allocate(wv(n_points), rh(n_points), ccov(n_points))
+    allocate(rlwp(n_points), cdre(n_points))
+    allocate(edout(n_points, lib%rows), esout(n_points, lib%rows))
+    allocate(taua_slice(n_points, lib%rows))
+    allocate(asymp_slice(n_points, lib%rows))
+    allocate(ssalb_slice(n_points, lib%rows))
 
+    ! Initialize points array
+    do i = 1, n_points
+        points(i) = i
+    end do
 
-    ! points = 1
+    ! Initialize calculation unit
+    calc = calc_unit(n_points, lib)
 
-!     sp=102377.13
-!     msl=102410.06
-!     d2m=284.10672
-!     t2m=286.96487
-!     tcc=12.03943
-!     ws10=10.0
-!     tclw=0.05
-!     tco3=0.0065
-!     cdrem=10.042508
-!     cldtcm=10.042508
+    ! Main computation loop
+    !---------------------
+    write(*, *) "Starting benchmark computations..."
     
-!     taua(1,:)=[0.12861817,0.11693237,0.11303711,0.10914185,0.10524658,0.10135132 &
-!     ,0.09745605,0.0934308,0.08906767,0.08519087,0.08188277,0.079188 &
-!     ,0.07697173,0.07506739,0.07332103,0.07159076,0.06982587,0.06804233 &
-!     ,0.0645105,0.05960191,0.05457829,0.05089346,0.0479044,0.04534442 &
-!     ,0.04311129,0.04106918,0.03914813,0.0373441,0.03567282,0.03332167 &
-!     ,0.02901247,0.01838189,0.00623265]
-!     asymp(1,:)=[0.78112185,0.76577055,0.76065344,0.7555364,0.75041926,0.74530214 &
-!     ,0.7401851,0.7352586,0.7308154,0.72576404,0.71973574,0.7124992 &
-!     ,0.7045832,0.69671303,0.68963724,0.68404067,0.6799671,0.6770385 &
-!     ,0.6733678,0.6695107,0.65929943,0.64360243,0.62599105,0.6091321 &
-!     ,0.59477234,0.58193725,0.5697906,0.5578721,0.54593724,0.5280586 &
-!     ,0.49310532,0.41066745,0.3164527]
-!     ssalb(1,:)=[0.9653854,0.9685926,0.96966165,0.9707307,0.9717998,0.97286886 &
-!     ,0.9739379,0.9750599,0.97628933,0.97734255,0.97821194,0.9788913 &
-!     ,0.9793933,0.97973263,0.9799186,0.9799566,0.9798687,0.9796886 &
-!     ,0.9791781,0.9784721,0.9785546,0.9787444,0.97678363,0.96918947 &
-!     ,0.95303714,0.9326887,0.91252154,0.897008,0.88944095,0.88677096 &
-!     ,0.89569014,0.89257544,0.88901585]
+    do iter = 1, n_iter
+        write(*, '(A,I0,A,I0)') "Processing iteration ", iter, " of ", n_iter
+        
+        ! Convert units and calculate derived variables for all points
+        do i = 1, n_points
+            ! Unit conversions
+            slp(i) = sp(i, iter) / 100.0d0    ! Pa to hPa
+            wsm(i) = ws10(i, iter)            ! m/s
+            oz(i) = tco3(i, iter)             ! Total column ozone
+            
+            ! Calculate relative humidity and precipitable water
+            T = t2m(i, iter)
+            Td = d2m(i, iter)
+            es_Td = a1 * exp(a3 * (Td - To) / (Td - a4))
+            es_T = a1 * exp(a3 * (T - To) / (T - a4))
+            rh(i) = 100.0d0 * es_Td / es_T
+            wv(i) = b1 * es_Td * (sp(i, iter)/100.0d0) / (msl(i, iter)/100.0d0) + b2
+            
+            ! Cloud properties
+            ccov(i) = tcc(i, iter)
+            rlwp(i) = tclw(i, iter) * 1000.0d0  ! Convert to g/m²
+            cdre(i) = cdrem(i, iter)
+            
+            ! Extract optical properties for this iteration
+            taua_slice(i, :) = taua(i, :, iter)
+            asymp_slice(i, :) = asymp(i, :, iter)
+            ssalb_slice(i, :) = ssalb(i, :, iter)
+        end do
+        
+        ! Call OASIM monrad function
+        call calc%monrad(points, iyr(iter), iday(iter), sec_b(iter), sec_e(iter), &
+                        sp(:, iter), msl(:, iter), ws10(:, iter), tco3(:, iter), &
+                        t2m(:, iter), d2m(:, iter), tcc(:, iter), tclw(:, iter), &
+                        cdrem(:, iter), taua_slice, asymp_slice, ssalb_slice, &
+                        edout, esout, error)
+        
+        if (error) then
+            write(*, *) "Error in OASIM calculation at iteration ", iter
+            stop 1
+        end if
+        
+    end do
 
-! ! Conversions
-!     slp = sp /100.0D0   
-!     wsm = ws10        ! m/s
-!     oz  = tco3
-    
-!  ! relative humidity and
-!  ! precipitable water (absorption by water vapour)  
- 
-    
-!     T            = t2m
-!     Td           = d2m 
-!     es_Td        = a1 * exp( a3 * (Td-To)/(Td-a4) )
-!     es_T         = a1 * exp( a3 *  (T-To)/(T-a4) )
-!     rh           = 100. * es_Td /es_T
-!     wv           = b1 * es_Td * (sp/100.)/(msl/100.) + b2
-    
-!     ccov = tcc
-    
-!     rlwp = tclw*1000.
-    
-!     cdre = cdrem
-    
-!    ! cldtc  = cldtcm
-    
-!     year = 2019
-!     day = 1
-!     sec_b = 28800.0d0
-!     sec_e = 36000.0d0
-!     calc = calc_unit(p_size, lib)
-!     call calc%monrad_debug(points, year, day, sec_b, sec_e, slp, wsm, oz, wv, & 
-!                           rh, ccov, rlwp, cdre, taua, asymp, ssalb, edout, esout, error)
-!     ! write(*, *) error
-!     write(*, *) "FINAL OUTPUT - direct"
-!     write(*, *) edout
-!     write(*, *) "FINAL OUTPUT - diffuse"
-!     write(*, *) esout      
-                     
-!     call calc%monrad(points, year, day, sec_b, sec_e, [sp], [msl], [ws10], [tco3], [t2m], [d2m], &
-!                      [tcc], [tclw], [cdrem], taua, asymp, ssalb, edout, esout, error)
+    write(*, *) "Benchmark completed successfully!"
 
-!     ! write(*, *) error
-!     write(*, *) "FINAL OUTPUT - direct"
-!     write(*, *) edout
-!     write(*, *) "FINAL OUTPUT - diffuse"
-!     write(*, *) esout  
-
-
-!     call calc%finalize()
-!     deallocate(slp)
-!     deallocate(wsm)
-!     deallocate(oz)
-!     deallocate(wv)
-!     deallocate(rh)
-!     deallocate(ccov)
-!     deallocate(rlwp)
-!     deallocate(cdre)
-!     deallocate(points)
-!     deallocate(taua)
-!     deallocate(asymp)
-!     deallocate(ssalb)
-!     deallocate(edout)
-!     deallocate(esout)
-!     call lib%finalize()
-!     if (allocated(lat)) deallocate(lat)
-!     if (allocated(lon)) deallocate(lon)
-
-    ! Clean up 
+    ! Clean up
     !---------
+    call calc%finalize()
+    call lib%finalize()
+    
     deallocate(iyr, iday)
     deallocate(sec_b, sec_e)
     deallocate(sp, msl, ws10, tco3, t2m)
     deallocate(d2m, tcc, tclw, cdrem)
     deallocate(taua, asymp, ssalb)
-
-    ! Closing the netcdf file 
-    !-------------------------
-    call check( nf90_close(ncid) )
+    deallocate(lat, lon)
+    deallocate(points)
+    deallocate(slp, wsm, oz, wv, rh, ccov, rlwp, cdre)
+    deallocate(edout, esout)
+    deallocate(taua_slice, asymp_slice, ssalb_slice)
 
     contains
         ! Simple subroutine to check the status of reading the netcdf file 
         subroutine check(status)
-            integer, intent ( in) :: status
+            integer, intent(in) :: status
             
             if(status /= nf90_noerr) then 
-            print *, trim(nf90_strerror(status))
-            stop "Stopped"
+                print *, trim(nf90_strerror(status))
+                stop "Stopped"
             end if
         end subroutine check  
 end program benchmark
