4c0b392dd5e2bc82ee67a8a0681f1ec730415d21
aredjil <bellred499@gmail.com>
2025-08-13 18:06:27 +0200

    changed ifort to ifx in the build script for compatibility with leonardo netcdf library

--- build_debug_intel.sh
+++ build_debug_intel.sh
@@ -7,4 +7,4 @@ CURR_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 rm -rf ${CURR_DIR}/${DEST_DIR}
 mkdir -p ${CURR_DIR}/${DEST_DIR}
 
-cd ${CURR_DIR}/${DEST_DIR} && cmake -D CMAKE_Fortran_COMPILER=ifort ../..
+cd ${CURR_DIR}/${DEST_DIR} && cmake -D CMAKE_Fortran_COMPILER=ifx ../..
